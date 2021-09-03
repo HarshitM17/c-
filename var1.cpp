eff59f60691843e53ec8c00db03b8d45485eb13d
HarshitM17 <mathurharshit88@gmail.com>
2021-09-03 20:45:42 +0530

    new

---
+++ var1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age=20;
+
+    cout<<"My name is Harshit and I am "<<age<<
+    " years old.";
+    return 0;
+
+}
---
+++ var2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num=90;
+    float cb=12.38;
+
+    cout<<"I paid "<<num<<" rupees online and got cashback of \n "<<cb<<" rupees.";
+
+    return 0;
+
+}
