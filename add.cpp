9e72d4b5d4b50b08c75178ab1b6b9136d21a1fbc
HarshitM17 <mathurharshit88@gmail.com>
2021-09-04 21:17:08 +0530

    push

---
+++ add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int num1,num2;
+
+    cout<<"Enter the first number-> ";
+    cin>>num1;
+
+    cout<<"Enter the second number-> ";
+    cin>>num2;
+
+    cout<<"The sum of 1st & 2nd number is-> "<<num1+num2;
+
+    return 0;
+
+
+}
--- var2.cpp
+++ var2.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 int main(){
-    int num=90;
-    float cb=12.38;
+    int num=9;
+    int kum=5;
+    float cb=1.38;
+    char n = 'H';
+    char s = 'M';
 
     cout<<"I paid "<<num<<" rupees online and got cashback of \n "<<cb<<" rupees.";
+    cout<<"\n Short form of nmy name is "<<n<<s;
+    cout<<"\n++num: "<<++num;
+    cout<<"\nnum++: "<<num++;
+    // cout<<"\n--num: "<<num;
+    // cout<<"\na--: "<<num--;
+    // cout<<"\n yo"<<(num==kum);
+    // cout<<"\nyu"<<(num>kum);
+
 
     return 0;
 
