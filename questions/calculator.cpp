9f14733dd1f1524cefac5d8e60338f48e44d8d16
HarshitM17 <mathurharshit88@gmail.com>
2022-04-06 20:12:16 +0530

    cal

---
+++ questions/calculator.cpp
@@ -0,0 +1,33 @@
+//Make a calculator using switch case.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter the first number: ";cin>>a;
+    cout<<"Enter the second number: ";cin>>b;
+    
+    char op;
+    cout<<"Enter the operation to get performed: ";cin>>op;
+
+    switch (op)
+    {
+    case '+':
+        cout<<"Answer is: "<<a+b<<endl;
+        break;
+    case '-':
+        cout<<"Answer is: "<<a-b<<endl;
+        break;
+    case '*':
+        cout<<"Answer is: "<<a*b<<endl;
+        break;
+    case '/':
+        cout<<"Answer is: "<<a/b<<endl;
+        break;
+    default:
+        cout<<"Not correct input!!";
+        break;
+    }
+    return 0;
+}
